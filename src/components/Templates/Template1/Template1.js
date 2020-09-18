efce4e77cfeaa8f4ad80aae390e49fdc53b7a405
SBhattacharya45 <umyadeepb27@gmail.com>
2020-09-18 20:54:16 +0530

    added dummy state in store, made template 1 dynamic

--- src/components/Templates/Template1/Template1.js
+++ src/components/Templates/Template1/Template1.js
@@ -45,18 +45,16 @@ class Template1 extends Component {
         )
 
         const skillsArray = this.props.skills;
-        let skills = skillsArray.join(', ')
+        let skills = skillsArray.map((skillElement, igKey) => (
+            <li key={igKey}>{skillElement.skill}</li>
+        ))
 
         const achievementArray = this.props.achivs;
-        let achievementItems = ( 
-            <div>
-                {achievementArray.map((achivsElement, igKey) => (
-                    <li>
-                        {achivsElement}                        
+        let achievementItems = achievementArray.map((achivsElement, igKey) => (
+                    <li key={igKey}>
+                        {achivsElement.achiv}                        
                     </li>
-                ))}
-            </div>
-        )
+                ))
     
         return(
 
@@ -111,7 +109,9 @@ class Template1 extends Component {
                         <h4>Skills</h4>
                     </div>
                     <div className={`${classes.data}  ${classes.skills }`}>
-                        {skills}
+                        <ul>
+                            {skills}
+                        </ul>
                     </div>
                 </section>
                 <section className={classes.section}>
@@ -133,40 +133,18 @@ class Template1 extends Component {
 
 const mapStateToProps = (state) => {
     return{
-        firstName : 'Rajat',
-        lastName : 'Chowdhury',
-        city: 'Kolkata',
-        state: 'WestBengal',
-        pincode : '123456',
-        email : 'crajat2000@gmail.com',
-        phoneNumber : '8017077130',
-        statement : 'Non aliqua pariatur aliquip et anim quis enim. Mollit minim labore sunt do duis consectetur labore incididunt qui sit fugiat. Tempor consequat enim culpa id adipisicing est. Sint veniam consectetur magna id reprehenderit tempor. Adipisicing eiusmod in ex dolore pariatur nisi nulla aliquip occaecat. Deserunt proident reprehenderit aute non exercitation id tempor id sint.',
-        eduDetails: [{
-            qualification: "B.Tech",
-            institute: 'Heritage Institute of Technology',
-            field: 'Electronics',
-            passout: '2022',
-            grade: '9'
-        },
-        {   qualification: "B.Tech",
-            institute: 'Heritage Institute of Technology',
-            field: 'Electronics',
-            passout: '2022',
-            grade: '9'}],
-        expDetails: [{
-            type: 'Internship',
-            organization: 'OrangeInk',
-            position: 'Web Developer',
-            duration: '2 months'
-        },
-        {
-            type: 'Internship',
-            organization: 'OrangeInk',
-            position: 'Web Developer',
-            duration: '2 months'
-        }],
-        skills : ['javascript' , 'css', 'html', 'bootstrap'],
-        achivs: ['last at everything' , 'rated worst for 3 years']
+        firstName : state.form.details.formValues[0].value,
+        lastName : state.form.details.formValues[1].value,
+        city: state.form.details.formValues[2].value,
+        state: state.form.details.formValues[3].value,
+        pincode : state.form.details.formValues[4].value,
+        email : state.form.details.formValues[5].value,
+        phoneNumber : state.form.details.formValues[6].value,
+        statement : state.form.details.formValues[7].value,
+        eduDetails: state.form.details.eduDetails,
+        expDetails: state.form.details.expDetails,
+        skills : state.form.details.skills,
+        achivs: state.form.details.achivs
 
     }
 }
--- src/store/reducers/form.js
+++ src/store/reducers/form.js
@@ -1,6 +1,111 @@
 
 const initialState = {
-    details: null
+    details: {
+        formValues: [
+          {
+            id: 0,
+            value: 'Soumya',
+            config: {
+              type: 'text',
+              placeholder: 'First Name'
+            }
+          },
+          {
+            id: 1,
+            inputType: 'input',
+            value: 'Bhattachraya',
+            config: {
+              type: 'text',
+              placeholder: 'Last Name'
+            }
+          },
+          {
+            id: 2,
+            inputType: 'input',
+            value: 'Kolkata',
+            config: {
+              type: 'text',
+              placeholder: 'City'
+            }
+          },
+          {
+            id: 3,
+            inputType: 'input',
+            value: 'West Bengal',
+            config: {
+              type: 'text',
+              placeholder: 'State'
+            }
+          },
+          {
+            id: 4,
+            inputType: 'input',
+            value: '700149',
+            config: {
+              type: 'text',
+              placeholder: 'Pincode'
+            }
+          },
+          {
+            id: 5,
+            inputType: 'input',
+            value: 'test@test.mail.com',
+            config: {
+              type: 'email',
+              placeholder: 'Enter your mail'
+            }
+          },
+          {
+            id: 6,
+            inputType: 'input',
+            value: '99999999',
+            config: {
+              type: 'text',
+              placeholder: 'Enter your phone number'
+            }
+          },
+          {
+            id: 7,
+            inputType: 'textarea',
+            value: 'i am iron man',
+            config: {
+              type: 'text',
+              placeholder: 'Enter your statement'
+            }
+          }
+        ],
+        eduDetails: [
+          {
+            qualification: 'B.Tech',
+            institute: 'Heritage Institute of Technology',
+            field: 'Electronics',
+            passout: '2022',
+            grade: '9'
+          }
+        ],
+        expDetails: [
+          {
+            type: 'Internship',
+            organization: 'OrangeInk',
+            position: 'Web Developer',
+            duration: '2 months'
+          }
+        ],
+        skills: [
+          {
+            skill: 'python'
+          },
+          {
+            skill: 'python'
+          }
+        ],
+        achivs: [
+          {
+            achiv: 'Topped in 3rd sem'
+          }
+        ],
+        imageUrl: 'blob:http://localhost:3000/09349018-13e3-4756-9ec4-759cefa29e83'
+      }
 }
 
 const updateDetails = (state, action) => {
