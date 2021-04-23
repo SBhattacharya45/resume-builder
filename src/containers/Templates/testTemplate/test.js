79bbd111b8b45a1b8671c3a3faa96fbd9bf17c73
Rajat-Chowdhury <crajat2000@gmail.com>
2021-04-23 13:01:41 +0530

    added table contents

--- src/containers/Templates/testTemplate/test.js
+++ src/containers/Templates/testTemplate/test.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { Page, Text, View, Document, StyleSheet, Link, Image } from '@react-pdf/renderer';
 import './test.css'
 
+
 // Create styles
 const styles = StyleSheet.create({
   page: {
     flexDirection: 'row',
-    backgroundColor: '#E4E4E4'
+    backgroundColor: 'whitesmoke',
+    fontSize: 12,
+
   },
   section: {
     margin: 10,
@@ -19,30 +22,87 @@ const styles = StyleSheet.create({
     borderStyle: "solid",
     borderWidth: 1,
     borderRightWidth: 0,
-    borderBottomWidth: 0
+    borderBottomWidth: 0,
+    
   },
   tableRow: {
     margin: "auto",
-    flexDirection: "row"
+    flexDirection: "row",
   },
   tableCol: {
     width: "20%",
     borderStyle: "solid",
     borderWidth: 1,
     borderLeftWidth: 0,
-    borderTopWidth: 0
+    borderTopWidth: 0,
+    
   },
   tableCol2: {
     width: "25%",
     borderStyle: "solid",
     borderWidth: 1,
     borderLeftWidth: 0,
-    borderTopWidth: 0
+    borderTopWidth: 0,
+   
+    
   },
   tableCell: {
     margin: "auto",
-    marginTop: 5,
+ 
     fontSize: 10
+  },
+  userCard: {
+
+    fontSize: 10,
+    paddingBottom: 5
+  },
+  heading: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    marginVertical: 10,
+    
+  },
+  projectCard: {
+    backgroundColor: 'white',
+    borderColor: 'black',
+    borderRadius: 8,
+    borderWidth: '5px',
+    borderStyle: 'solid',
+
+
+
+  },
+  button: {
+    display: 'flex',
+    borderTopColor: 'grey',
+    borderTopWidth: '5px',
+    color: '#4285f4',
+    borderTopStyle: "solid",
+    fontSize: 10,
+    padding: 5,
+
+    textDecoration: 'none',
+
+
+    textAlign: 'center'
+
+  },
+  tableHeaders: {
+    backgroundColor:'#4285f4',
+    color: ' white',
+    padding: 5,
+    
+    
+    
+  },
+  tableContents : {
+    padding: 5,
+    backgroundColor:'white'
+  },
+  bulletPoints:{
+    fontSize: 10,
+    paddingBottom: 5
+
   }
 });
 
@@ -51,19 +111,19 @@ const MyDocument = (props) => {
 
   const eduList = props.userDetails.eduDetails.map((eduObject, igKey) => (
     <View id={igKey} style={styles.tableRow}>
-      <View style={styles.tableCol}>
+      <View style={[styles.tableCol,styles.tableContents]}>
         <Text style={styles.tableCell}>{eduObject.qualification}</Text>
       </View>
-      <View style={styles.tableCol}>
+      <View style={[styles.tableCol,styles.tableContents]}>
         <Text style={styles.tableCell}>{eduObject.field}</Text>
       </View>
-      <View style={styles.tableCol}>
+      <View style={[styles.tableCol,styles.tableContents]}>
         <Text style={styles.tableCell}>{eduObject.institute}</Text>
       </View>
-      <View style={styles.tableCol}>
+      <View style={[styles.tableCol,styles.tableContents]}>
         <Text style={styles.tableCell}>{eduObject.passout}</Text>
       </View>
-      <View style={styles.tableCol}>
+      <View style={[styles.tableCol,styles.tableContents]}>
         <Text style={styles.tableCell}>{eduObject.grade}</Text>
       </View>
     </View>
@@ -71,35 +131,37 @@ const MyDocument = (props) => {
 
   const expList = props.userDetails.expDetails.map((expObject, igKey) => (
     <View id={igKey} style={styles.tableRow}>
-      <View style={styles.tableCol2}>
+      <View style={[styles.tableCol2, styles.tableContents]}>
         <Text style={styles.tableCell}>{expObject.type}</Text>
       </View>
-      <View style={styles.tableCol2}>
+      <View style={[styles.tableCol2, styles.tableContents]}>
         <Text style={styles.tableCell}>{expObject.organization}</Text>
       </View>
-      <View style={styles.tableCol2}>
+      <View style={[styles.tableCol2, styles.tableContents]}>
         <Text style={styles.tableCell}>{expObject.position}</Text>
       </View>
-      <View style={styles.tableCol2}>
+      <View style={[styles.tableCol2, styles.tableContents]}>
         <Text style={styles.tableCell}>{expObject.duration}</Text>
       </View>
     </View>
   ))
 
   const skills = props.userDetails.skills.map((skillObj, igKey) => (
-    <Text id={igKey}>&bull;&nbsp;{skillObj.skill}</Text>
+    <Text id={igKey} style={styles.bulletPoints}>&bull;&nbsp;{skillObj.skill}</Text>
   ))
 
   const achievements = props.userDetails.achivs.map((achivObj, igKey) => (
-    <Text id={igKey}>&bull;&nbsp;{achivObj.achiv}</Text>
+    <Text id={igKey} style={styles.bulletPoints}>&bull;&nbsp;{achivObj.achiv}</Text>
   ))
 
   const projectDetails = props.userDetails.proDetails.map((projectObj, igKey) => (
-    <View>
-      <Text id={igKey} > {projectObj.type} </Text>
-      <Text id={igKey} > {projectObj.name} </Text>
-      <Text id={igKey} > {projectObj.description} </Text>
-      <Link src={projectObj.link}>Click here to view</Link>
+    <View style={styles.projectCard}>
+      <Text id={igKey} style={{ fontSize: 12, marginLeft: 10, marginTop: 10, marginBottom : 5 }}> {projectObj.name} </Text>
+      <Text id={igKey} style={{ fontSize: 10, color: 'grey', marginLeft: 10, marginVertical: 5 }}> {projectObj.type} </Text>
+      <Text id={igKey} style={{ fontSize: 12, marginLeft: 10, marginVertical: 5 }}> {projectObj.description} </Text>
+      <View style={{ height: ' 1px', backgroundColor: 'lightgrey' }} />
+      <Link src={projectObj.link} style={styles.button}>Click here to view</Link>
+
     </View>
 
 
@@ -107,16 +169,16 @@ const MyDocument = (props) => {
 
   const training = props.userDetails.trainDetails.map((trainObj, igKey) => (
     <View id={igKey} style={styles.tableRow}>
-      <View style={styles.tableCol2}>
+      <View style={[styles.tableCol2, styles.tableContents]}>
         <Text style={styles.tableCell}>{trainObj.type}</Text>
       </View>
-      <View style={styles.tableCol2}>
+      <View style={[styles.tableCol2, styles.tableContents]}>
         <Text style={styles.tableCell}>{trainObj.from}</Text>
       </View>
-      <View style={styles.tableCol2}>
+      <View style={[styles.tableCol2, styles.tableContents]}>
         <Text style={styles.tableCell}>{trainObj.to}</Text>
       </View>
-      <View style={styles.tableCol2}>
+      <View style={[styles.tableCol2, styles.tableContents]}>
         <Text style={styles.tableCell}>{trainObj.description}</Text>
       </View>
     </View>
@@ -128,19 +190,19 @@ const MyDocument = (props) => {
     <Document>
       <Page size="A4" style={styles.page}>
         <View style={styles.section}>
-          <View stule={{display:'flex'}}>
+          <View style={{ display: 'flex' }}>
             <View>
-              <Text>
+              <Text style={[styles.userCard, styles.heading]}>
                 {props.userDetails.formValues[0].value + " " + props.userDetails.formValues[1].value}
               </Text>
-              <Text>
+              <Text style={styles.userCard}>
                 {props.userDetails.formValues[2].value}, {props.userDetails.formValues[3].value}
               </Text>
-              <Text>{props.userDetails.formValues[4].value}</Text>
-              <Text>
+              <Text style={styles.userCard}>{props.userDetails.formValues[4].value}</Text>
+              <Text style={styles.userCard}>
                 Mobile: {props.userDetails.formValues[6].value}
               </Text>
-              <Text>
+              <Text style={styles.userCard}>
                 Email: {props.userDetails.formValues[5].value}
               </Text>
             </View>
@@ -152,22 +214,22 @@ const MyDocument = (props) => {
 
           <Text>&nbsp;</Text>
           <View>
-            <Text>Educational Details</Text>
+            <Text style={styles.heading}>Educational Details</Text>
             <View style={styles.table}>
               <View style={styles.tableRow}>
-                <View style={styles.tableCol}>
+                <View style={[styles.tableCol, styles.tableHeaders]}>
                   <Text style={styles.tableCell}>Qualification</Text>
                 </View>
-                <View style={styles.tableCol}>
+                <View style={[styles.tableCol, styles.tableHeaders]}>
                   <Text style={styles.tableCell}>Field</Text>
                 </View>
-                <View style={styles.tableCol}>
+                <View style={[styles.tableCol, styles.tableHeaders]}>
                   <Text style={styles.tableCell}>Institute</Text>
                 </View>
-                <View style={styles.tableCol}>
+                <View style={[styles.tableCol, styles.tableHeaders]}>
                   <Text style={styles.tableCell}>Passout</Text>
                 </View>
-                <View style={styles.tableCol}>
+                <View style={[styles.tableCol, styles.tableHeaders]}>
                   <Text style={styles.tableCell}>Grade</Text>
                 </View>
               </View>
@@ -176,19 +238,19 @@ const MyDocument = (props) => {
           </View>
           <Text>&nbsp;</Text>
           <View>
-            <Text>Experience</Text>
+            <Text style={styles.heading}>Experience</Text>
             <View style={styles.table}>
               <View style={styles.tableRow}>
-                <View style={styles.tableCol2}>
+                <View style={[styles.tableCol2, styles.tableHeaders]}>
                   <Text style={styles.tableCell}>Type</Text>
                 </View>
-                <View style={styles.tableCol2}>
+                <View style={[styles.tableCol2, styles.tableHeaders]}>
                   <Text style={styles.tableCell}>Oraganization</Text>
                 </View>
-                <View style={styles.tableCol2}>
+                <View style={[styles.tableCol2, styles.tableHeaders]}>
                   <Text style={styles.tableCell}>Position</Text>
                 </View>
-                <View style={styles.tableCol2}>
+                <View style={[styles.tableCol2, styles.tableHeaders]}>
                   <Text style={styles.tableCell}>Duration</Text>
                 </View>
               </View>
@@ -197,34 +259,34 @@ const MyDocument = (props) => {
           </View>
           <Text>&nbsp;</Text>
           <View>
-            <Text>Skills</Text>
+            <Text style={styles.heading}>Skills</Text>
             {skills}
           </View>
           <Text>&nbsp;</Text>
           <View>
-            <Text>Achievements</Text>
+            <Text style={styles.heading}>Achievements</Text>
             {achievements}
           </View>
           <Text>&nbsp;</Text>
           <View>
-            <Text>Projects</Text>
+            <Text style={styles.heading}>Projects</Text>
             {projectDetails}
           </View>
           <Text>&nbsp;</Text>
-          <Text>Training</Text>
+          <Text style={styles.heading}>Training</Text>
           <View style={styles.table}>
             <View style={styles.tableRow}>
-              <View style={styles.tableCol2}>
+              <View style={[styles.tableCol2, styles.tableHeaders]}>
                 <Text style={styles.tableCell}>Type</Text>
               </View>
 
-              <View style={styles.tableCol2}>
+              <View style={[styles.tableCol2, styles.tableHeaders]}>
                 <Text style={styles.tableCell}>From</Text>
               </View>
-              <View style={styles.tableCol2}>
+              <View style={[styles.tableCol2, styles.tableHeaders]}>
                 <Text style={styles.tableCell}>To</Text>
               </View>
-              <View style={styles.tableCol2}>
+              <View style={[styles.tableCol2, styles.tableHeaders]}>
                 <Text style={styles.tableCell}>Description</Text>
               </View>
             </View>
